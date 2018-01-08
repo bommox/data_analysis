d46153fe1222ae6bb50e33720ba267ed7fbc5015
jblomdah <jorge.blom-dahl-oliver@capgemini.com>
2018-01-08 18:20:58 +0100

    Using lodash to analyze data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const Candle = require('./core').Candle
 const bluebird = require('bluebird')
 const fs = require('fs')
+const _ = require('lodash')
+
 
 // Async utils
 const readFile = bluebird.Promise.promisify(fs.readFile)
@@ -10,7 +12,7 @@ async function init() {
     let data;
     try {
         console.log("Reading data...")
-        data = JSON.parse(await readFile('../test/dax-1h.json'))
+        data = JSON.parse(await readFile('../test/dax-1m.json'))
         console.log("Data read!")
         await processData(data)
     } catch (e) {
@@ -30,6 +32,12 @@ async function processData(jsonData) {
     if (!jsonData && !jsonData.candles) throw 'No candles'
     const candles = jsonData.candles.map(rawT21 => Candle.fromT21(rawT21))
     candles.forEach(c => console.log(c.diff()))
+    let min = _.minBy(candles, 'open')
+    console.log("El open minimo es " + min.resumen())
+    let max = _.maxBy(candles, 'open')
+    console.log("El open maximo es " + max.resumen())
+    let diffMax =  _.maxBy(candles, c => Math.abs( c.diff() ))
+    console.log("La diferencia maximo es " + diffMax.resumen())
 }
 
 init()
